52d1e4cf54b68d20ce97e2676b94bdbddd3fd770
SAAHIL-5 <tomarsaahil@gmail.com>
2026-05-04 02:20:11 +0530

    final fix

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  preview: {
+    host: true,
+    port: 8080,
+    allowedHosts: "all"
+  }
 })
