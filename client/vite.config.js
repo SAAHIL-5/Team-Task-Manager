75a5c73a2e8854c576b48c94e7f64fb620610dbf
SAAHIL-5 <tomarsaahil@gmail.com>
2026-05-04 01:43:20 +0530

    final fix

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,4 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: true,
+    port: 8080,
+    allowedHosts: [
+      'team-task-manager-production-4ba6.up.railway.app'
+    ]
+  }
 })
