d8cdb4e957704e25509d4bcb7fc9cf637c045b9c
SAAHIL-5 <tomarsaahil@gmail.com>
2026-05-04 02:26:20 +0530

    final fix

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   preview: {
     host: true,
     port: 8080,
-    allowedHosts: "all"
+    allowedHosts: [
+      'team-task-manager-production-4ba6.up.railway.app'
+    ]
   }
 })
